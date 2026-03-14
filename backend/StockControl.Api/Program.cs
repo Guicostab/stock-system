191efb2b45c79bb5c0f615c82e360a94a637dea6
Guilherme Costa <guicostab@gmail.com>
2026-03-14 09:47:19 -0300

    Novo Projeto .NET

---
+++ backend/StockControl.Api/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
